2cfe787cfea1bcd3cd5c95ff639edfcba69a2d6f
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-02-11 16:44:07 +0100

    Move preview props into separate manifest

--- packages/next/src/build/templates/app-page-runtime.ts
+++ packages/next/src/build/templates/app-page-runtime.ts
@@ -258,6 +258,7 @@ export function createAppPageEntrypoint({
       interceptionRoutePatterns,
       deploymentId,
       clientAssetToken,
+      previewProps,
     } = prepareResult
 
     let { isOnDemandRevalidate } = prepareResult
@@ -805,6 +806,7 @@ export function createAppPageEntrypoint({
         (await routeModule.getIncrementalCache(
           req,
           nextConfig,
+          previewProps,
           prerenderManifest,
           isMinimalMode
         ))
@@ -891,7 +893,7 @@ export function createAppPageEntrypoint({
             crossOrigin: nextConfig.crossOrigin,
             trailingSlash: nextConfig.trailingSlash,
             images: nextConfig.images,
-            previewProps: prerenderManifest.preview,
+            previewProps,
             enableTainting: nextConfig.experimental.taint,
             reactMaxHeadersLength: nextConfig.reactMaxHeadersLength,
 
@@ -1238,6 +1240,7 @@ export function createAppPageEntrypoint({
                 nextConfig,
                 routeKind: RouteKind.APP_PAGE,
                 isFallback: true,
+                previewProps,
                 prerenderManifest,
                 isRoutePPREnabled,
                 responseGenerator: async () =>
@@ -1608,6 +1611,7 @@ export function createAppPageEntrypoint({
           isRoutePPREnabled,
           req,
           nextConfig,
+          previewProps,
           prerenderManifest,
           waitUntil: ctx.waitUntil,
           isMinimalMode,
